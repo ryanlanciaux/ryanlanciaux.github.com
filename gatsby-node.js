341776b735d0231ca07311cda494abbe2332f6f4
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2019-04-25 22:02:07 -0400

    Paginate blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,7 +48,22 @@ exports.createPages = ({ graphql, actions }) => {
       })
     })
 
-    return null
+    // Create blog post list pages
+    const postsPerPage = 2
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/` : `/${i + 1}`,
+        component: path.resolve("./src/templates/blog-list.js"),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1,
+        },
+      })
+    })
   })
 }
 
