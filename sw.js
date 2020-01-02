961404f2bd78bcb68b6d3a6cb0348ede624c3d1f
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2020-01-02 08:23:42 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e71df17ab0056cb12d48.js"
+    "url": "webpack-runtime-652eee548ba7a151727e.js"
   },
   {
     "url": "styles.8eb235527c003dbbd154.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4070743c06a728fdfd35b7ec751dbee2"
+    "revision": "8c8f824bfce383126e2b1065b09ab3fc"
   },
   {
     "url": "1-ad6d1288e8be7e12da61.js"
